bd42273441493cd7bd7287e4851d1b6d8a5eca19
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-07-26 19:48:00 +0530

    Creating express server and connecting mysql

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database:"attendance"
+})
+
+
+
+app.get("/", (req,res) => {
+    res.json("hello this is the backend")
+})
+
+
+app.listen(8800, () => {
+    
+    console.log("Connected to backend")
+
+})
