7f6bf718cff966b58862b1080329f6a0de2fcb05
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-04 00:12:51 +0530

    Creating React app and Routes to pages

---
+++ frontend/attendance-marking-app/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Registration from './pages/Registration';
+import People from './pages/People';
+import Attendance from './pages/Attendance';
+import Update from './pages/Update';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/registration" element={ <Registration/>} />
+          <Route path="/people" element={ <People/>} />
+          <Route path="/attendance" element={ <Attendance/>} />
+          <Route path="/update" element={ <Update/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
