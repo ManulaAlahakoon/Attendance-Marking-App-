ca121cb5c02f451c345819618463fb2722396cb6
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-05 15:12:53 +0530

    Adding 'cors' middle wear
    cross origin resource sharing

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
 import express from "express"
 import mysql from "mysql"
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 //By default , we cannot send any data to express server through client. To prevent that we use this middlewear
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database:"attendance"
-})
+app.use(cors())
+//By default , Backend server does not allow any applicattion to use backend api .To prevent that we use the cors middlewear
+/*
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+*/
 
+    const db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "attendance"
+    })
 
 app.get("/", (req, res) => {
     res.json("Conncted")
