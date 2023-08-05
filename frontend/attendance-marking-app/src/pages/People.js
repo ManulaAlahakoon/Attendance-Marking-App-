fd9d1c643286608bfad5c227a0c6f593775581a7
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-05 15:15:33 +0530

    Fetching data from the backend

--- frontend/attendance-marking-app/src/pages/People.js
+++ frontend/attendance-marking-app/src/pages/People.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const People = () => {
+
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    
+    const fetchAllPeople = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/people")
+        setPeople(res.data)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    fetchAllPeople();
+  },[])
   return (
-    <div>People</div>
+    <div className="PeopleDetail">
+      
+      <div className="peopleDetail">
+        
+        {people.map(person => (
+
+          <div className="person" key={ person.registration_id}>
+
+            <h3>{person.name}</h3>
+            <h4>{person.registration_id}</h4>
+            <p>{person.city}</p>
+            <p>{ person.phone_number}</p>
+
+          </div>
+          
+        ))} 
+        
+    </div>
+    </div>
+    
   )
 }
 
